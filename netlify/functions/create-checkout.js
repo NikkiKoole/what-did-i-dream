f7f75e44a861e424f87888ad2d71fc852fae9d54
Nikki Koole <nikkikoole@gmail.com>
2025-04-24 08:14:20 +0200

    testing in production

--- netlify/functions/create-checkout.js
+++ netlify/functions/create-checkout.js
@@ -3,23 +3,26 @@ import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export async function handler(event) {
-  const { priceId } = JSON.parse(event.body);
+  console.log(event);
+  try {
+    const { priceId } = JSON.parse(event.body);
 
-  const session = await stripe.checkout.sessions.create({
-    mode: "payment",
-    payment_method_types: ["card"],
-    line_items: [
-      {
-        price: priceId,
-        quantity: 1,
-      },
-    ],
-    success_url: `${process.env.SITE_URL}/success`,
-    cancel_url: `${process.env.SITE_URL}/cancel`,
-  });
+    const session = await stripe.checkout.sessions.create({
+      mode: "payment",
+      payment_method_types: ["card"],
+      line_items: [{ price: priceId, quantity: 1 }],
+      success_url: `${process.env.SITE_URL}/success`,
+      cancel_url: `${process.env.SITE_URL}/cancel`,
+    });
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ url: session.url }),
-  };
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ url: session.url }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: error.message }),
+    };
+  }
 }
