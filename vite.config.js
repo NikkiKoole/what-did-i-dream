e3ade438760ceb34dbcae09845abf6208f212fe0
Nikki Koole <nikkikoole@gmail.com>
2025-04-26 14:58:51 +0200

    humpa

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,11 @@ export default defineConfig({
   base: "/",
   build: {
     target: "esnext", // Allow top-level await and modern features
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        journal: "public/journal.html",
+      },
+    },
   },
 });
