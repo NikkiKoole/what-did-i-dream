2d0b1fbafa11bfea4dfb7194288e35c3bb0e9910
Nikki Koole <nikkikoole@gmail.com>
2025-04-26 15:02:35 +0200

    journal page

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,16 @@
 // vite.config.js
 import { defineConfig } from "vite";
-
+import { resolve } from "path";
 export default defineConfig({
   base: "/",
   build: {
     target: "esnext", // Allow top-level await and modern features
     rollupOptions: {
       input: {
-        main: "index.html",
-        journal: "public/journal.html",
+        main: resolve(__dirname, "index.html"),
+        journal: resolve(__dirname, "journal.html"),
+        privacy: resolve(__dirname, "privacy.html"),
+        terms: resolve(__dirname, "terms.html"),
       },
     },
   },
